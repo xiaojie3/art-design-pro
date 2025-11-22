172d83ad2b5b5635e98b09183f0a226216226720
xiaojie3 <961334527@qq.com>
2025-11-22 14:36:46 +0800

    refactor: 删除控制台

--- src/router/modules/index.ts
+++ src/router/modules/index.ts
@@ -1,9 +1,8 @@
 import { AppRouteRecord } from '@/types/router'
-import { dashboardRoutes } from './dashboard'
 import { systemRoutes } from './system'
 import { devToolsRoutes } from './devTools'
 
 /**
  * 导出所有模块化路由
  */
-export const routeModules: AppRouteRecord[] = [dashboardRoutes, systemRoutes, devToolsRoutes]
+export const routeModules: AppRouteRecord[] = [systemRoutes, devToolsRoutes]
